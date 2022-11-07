a3b81b5398449a17b632e3af26f8fff0ef759e54
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-11-07 20:28:02 -0300

    adjusting Text component

--- lab-ds/src/components/Text.tsx
+++ lab-ds/src/components/Text.tsx
@@ -1,13 +1,18 @@
+import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
+import {ReactNode} from 'react';
 
 export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
-    children: string;
+    children: ReactNode;
+    asChild?: boolean;
 }
 
-export function Text({ size = 'md', children }: TextProps) {
+export function Text({ size = 'md', children, asChild }: TextProps) {
+    const Component = asChild ? Slot : 'span';
+
     return (
-        <span className={clsx(
+        <Component className={clsx(
             'text-gray-100 font-sans',
             {
                 'text-xs': size === 'sm',
@@ -17,6 +22,6 @@ export function Text({ size = 'md', children }: TextProps) {
         )}
         >
             {children}
-        </span>
+        </Component>
     );
 }
