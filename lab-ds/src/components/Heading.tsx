e8a8624fbb51a661b2983693a70a09ddd394e1f8
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-11-07 21:29:52 -0300

    Create TextInput Component

--- lab-ds/src/components/Heading.tsx
+++ lab-ds/src/components/Heading.tsx
@@ -1,6 +1,6 @@
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
-import {ReactNode} from 'react';
+import { ReactNode } from 'react';
 
 export interface HeadingProps {
     size?: 'sm' | 'md' | 'lg';
